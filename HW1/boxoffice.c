7b57c47b658a0d3e480c783bc7fd3684eb4b0552
khoai <khanhphuongdo28@gmail.com>
2024-02-03 00:11:04 -0600

    initialize file

---
+++ HW1/boxoffice.c
@@ -0,0 +1,18 @@
+/*
+  Author: Khanh Do                  
+  Class: CSC-161-01                  
+ */
+
+/***************************************/
+/*            TICKET PRICING           */
+/*    This program will calculate the  */
+/*   purchases of tickets at a theater */
+/***************************************/
+
+#include <stdio.h>
+
+int main(void) {
+
+
+    return 0;
+}
