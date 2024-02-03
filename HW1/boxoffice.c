c1c22c20390ae8074c5e09db5093f60cb31a67c5
khoai <khanhphuongdo28@gmail.com>
2024-02-03 01:22:27 -0600

    handle invalid inputs

--- HW1/boxoffice.c
+++ HW1/boxoffice.c
@@ -32,22 +32,41 @@ int main(void) {
 
     // Number of tickets input
     printf("Welcome to The Strand!\nEnter the number of tickets you would like to purchase: ");
-    scanf("%d", num_ticket);
+    scanf("%d", &num_ticket);
 
     for (int i = 1; i <= num_ticket; i++) {
-        // Location input
-        printf("Which location would you like to buy ticket for?\nType '1' for Main floor-Middle Section\nType '2' for Main floor-sides\nType '3' for Balcony\nChoose here: ");
-        scanf("%d", &location);
-
-        // Seat input
-        printf("Please select show time:\nType '1' to select Matinee\nType '2' to select Evening\nChoose here: ");
-        scanf("%d", &time);
-
         // Age input
         printf("Please enter the age of ticket holder: ");
         scanf("%d", &age);
 
-        
+        // Handle Age Invalid Inputs
+        if ((age < 0) || (age > 150)) {
+            printf("Invalid age. Age is between 0 and 150. Please try again!");
+            i--;
+            continue;
+        }
+
+        // Location input
+        printf("Which location would you like to buy ticket for?\nType '1' for Main floor-Middle Section\nType '2' for Main floor-sides\nType '3' for Balcony\nChoose here: ");
+        scanf("%d", &location);
+
+        // Handle Location Invalid Inputs
+        if ((location != 1 || location != 2) || location != 3) {
+            printf("Invalid location. Pick from 1, 2, or 3. Please try again!");
+            i--;
+            continue;
+        }
+
+        // Seat input
+        printf("Please select show time:\nType '1' to select Matinee\nType '2' to select Evening\nChoose here: ");
+        scanf("%d", &time);    
+
+        // Handle Location Invalid Inputs
+        if ((time != 1 || time != 2) || time != 3) {
+            printf("Invalid time. Pick from 1, 2, or 3. Please try again!");
+            i--;
+            continue;
+        } 
     }
 
     printf("Total Price: %.2f dollars.", total_price);
