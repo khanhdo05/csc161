4f0272dc23c3122ba58cb84737e1097bbac4f190
khoai <khanhphuongdo28@gmail.com>
2024-02-03 00:14:50 -0600

    ask for input ticket

--- HW1/boxoffice.c
+++ HW1/boxoffice.c
@@ -13,6 +13,17 @@
 
 int main(void) {
 
+    /* 
+        Valid ages for ticket holders are between 0 and 150 (inclusive). 
+        Tickets for children 5 and under are free regardless of time and location. 
+        Children between 6 and 10, inclusive, and senior citizens (age 55 and older) receive a $5.00 discount.
+    */
+
+    int num_ticket;
+
+    // Input Handling
+    printf("Enter the number of tickets you would you like to purchase: ");
+    scanf("%d", num_ticket);
 
     return 0;
 }
