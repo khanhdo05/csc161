2be06ec2ec861b48de8979dabd46378a38e311b9
khoai <khanhphuongdo28@gmail.com>
2024-02-10 10:39:40 -0600

    initialize stdio.h

---
+++ HW3/creditcard.c
@@ -0,0 +1,15 @@
+/*
+  Author: Khanh Do                  
+  Class: CSC-161-01                  
+ */
+
+/***************************************/
+/*   CREDIT CARD INTEREST CALCULATOR   */
+/*                  ~                  */
+/*    This program will simulate       */
+/*  calculating credit card interest   */
+/*  whilst making only the minimum     */
+/*         payment each month.         */
+/***************************************/
+
+#include <stdio.h>
