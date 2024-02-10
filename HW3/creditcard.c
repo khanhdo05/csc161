4407f5a25332d0f546c0407911e1675b703d8243
khoai <khanhphuongdo28@gmail.com>
2024-02-10 11:10:49 -0600

    main function, initialize values and ask for inputs

--- HW3/creditcard.c
+++ HW3/creditcard.c
@@ -8,5 +8,32 @@
 #include <math.h>
 
 int main() {
-  return 0;
+
+    // Assigning constant values
+    const int APR = 18; // percents
+    const double PAYMENT = 35.00; // dollars per month
+    const double INITIAL = 500.00; // dollars
+
+    // Initializing variables
+    int days, total_month, start_month, cycle_end;
+    float interest;
+    float balance = INITIAL;
+    float total_payment;
+
+    // Welcome text
+    printf("Current APR = %d%, monthly payment = $%f, initial payment = $%f\n", APR, PAYMENT, INITIAL);
+
+    // Ask for user's input of starting month
+    printf("Which month are you starting at: ");
+    scanf("%d", start_month);
+    printf("For how many months: ");
+    scanf("%d", total_month);
+    cycle_end = total_month - 1;
+
+    // For loop to print the format
+
+    // Conclusion text
+    printf("After %d months, you paid $%f on an initial balance of $%f", total_month, total_payment, INITIAL);
+    
+    return 0;
 }
