903ea30fcbe4ec1fc887b41b74c45cecb67b38e8
khoai <khanhphuongdo28@gmail.com>
2024-02-10 11:12:54 -0600

    reformat and debug

--- HW3/creditcard.c
+++ HW3/creditcard.c
@@ -21,19 +21,19 @@ int main() {
     float total_payment;
 
     // Welcome text
-    printf("Current APR = %d%, monthly payment = $%f, initial payment = $%f\n", APR, PAYMENT, INITIAL);
+    printf("Current APR = %d, monthly payment = $%.2f, initial payment = $%.2f\n", APR, PAYMENT, INITIAL);
 
     // Ask for user's input of starting month
     printf("Which month are you starting at: ");
-    scanf("%d", start_month);
+    scanf("%d", &start_month);
     printf("For how many months: ");
-    scanf("%d", total_month);
+    scanf("%d", &total_month);
     cycle_end = total_month - 1;
 
     // For loop to print the format
 
     // Conclusion text
-    printf("After %d months, you paid $%f on an initial balance of $%f", total_month, total_payment, INITIAL);
+    printf("After %d months, you paid $%.2f on an initial balance of $%.2f\n", total_month, total_payment, INITIAL);
     
     return 0;
 }
