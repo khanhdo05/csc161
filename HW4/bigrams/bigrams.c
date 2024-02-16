abacdb6b63523a96da82489f0b36549b6a015ec4
khoai <khanhphuongdo28@gmail.com>
2024-02-15 20:36:27 -0600

    kinda create the array ish

--- HW4/bigrams/bigrams.c
+++ HW4/bigrams/bigrams.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 
 int main() {
-  // TODO: Complete your implementation here
+  // Initiate variables
+  int len = 0;
+  char ch;
+  char message[len];
+
+  while ((ch = getchar()) != '\n') {
+    message[len] = ch;
+    len++;
+  }
+
+  char arr[2][len-1];
+  
+  // Initialize the array with '0' in the second column
+  for (int i = 0; i < 2; i++) {
+      for (int j = 0; j < len - 1; j++) {
+          if (j == 1) { // Set the second column to '0'
+              arr[i][j] = '0';
+          } else {      // Set other columns as desired, here I'm just using a placeholder character 'x'
+              arr[i][j] = 'x';
+          }
+      }
+  }
+
+  // Print the most common bigram
+  printf("Most common bigram: 'bigram'\n");
 
   return 0;
 }
