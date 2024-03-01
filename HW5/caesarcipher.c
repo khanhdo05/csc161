ef0dfff268ab83feacaced963bcdba37133dd7df
khoai <khanhphuongdo28@gmail.com>
2024-03-01 13:51:12 -0600

    structure of the program

--- HW5/caesarcipher.c
+++ HW5/caesarcipher.c
@@ -4,3 +4,36 @@
  * 
  * \author Khanh Do
  */
+
+#include <stdio.h>
+#define MAX_LENGTH 100
+
+// Helper functions
+// A function that do the encoding process to 1 char a
+// A function that do the decoding process to 1 char b
+// A function that either takes in a or b to do the encoding/decoding process to the whole message by returning the table
+// A function that takes in the function that encode/decode the whole message to print out the table
+
+int main(void) {
+
+    // Initialize variables
+
+    // Print welcoming message
+    printf("This program encrypts and decrypts messages using the Caesar Cipher.\n");
+    printf("Type e to encode or d to decode a message: ");
+    // Boolean to see if getchar() records an 'e' or a 'd' and run that following function.
+    /* if ((ch = getchar()) == 'e') {
+        printf("Enter the string to encode: ");
+        encode_function;
+    } else if ((ch = getchar()) == 'd') {
+        printf("Enter the string to decode: ");
+    } else {
+        printf("Valid options are 'e' to encode or 'd' to decode");
+        return 0;
+    }
+    */
+
+    // For loop to print out the table
+
+    return 0;
+}
