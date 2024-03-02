412e9e02ac16985febaf8a0896098db95a5cb741
khoai <khanhphuongdo28@gmail.com>
2024-03-01 19:54:07 -0600

    finish project and tests

--- HW5/caesarcipher.c
+++ HW5/caesarcipher.c
@@ -1,6 +1,6 @@
 
 /**
- * A program to encipher and decipher Caesar message.
+ * A program to encipher and decipher Caesar code.
  * 
  * \author Khanh Do
  */
@@ -8,12 +8,10 @@
 #include <stdio.h>
 #define MAX_LENGTH 100
 
-// Helper functions
-// A function that check if input message is correct: lowercase, char, no symbol or num
+// Helper functions declarations
 char encodeChar(char ch, int key);
 char decodeChar(char ch, int key);
-// A function that either takes in a or b to do the encoding/decoding process to the whole message by returning the table
-// A function that takes in the function that encode/decode the whole message to print out the table
+void processMessage(char input[], char output[], int len, int key, char mode);
 
 // Function to encode a character using the Caesar cipher
 char encodeChar(char ch, int key) {
@@ -25,35 +23,57 @@ char decodeChar(char ch, int key) {
     return ((ch - 'a' - key + 26) % 26) + 'a';
 }
 
+// Function to encode or decode the input message based on mode
+void processMessage(char input[], char output[], int len, int key, char mode) {
+    for (int i = 0; i < len; i++) {
+        if (mode == 'e') { // Encode
+            output[i] = encodeChar(input[i], key);
+        } else if (mode == 'd') { // Decode
+            output[i] = decodeChar(input[i], key);
+        } 
+    }
+    output[len] = '\0'; // Output is null-terminated
+}
+
 int main(void) {
 
     // Initialize variables
-    char arr_codes[26] = {0};
     int len = 0;
-    char ch;
+    char arr_message[MAX_LENGTH], temp_message[MAX_LENGTH];
+    char ch, mes;
 
-    // Print welcoming message and read in user input
+    // Print welcoming message
     printf("This program encrypts and decrypts messages using the Caesar Cipher\n");
+
+    // Read in what mode user want to use
     printf("Type e to encode or d to decode a message: ");
     ch = getchar();
-    // Boolean to see if getchar() records an 'e' or a 'd' and run that following function.
-    if (ch == 'e') {
-        printf("Enter the string to encode: ");
-    } else if (ch == 'd') {
-        printf("Enter the string to decode: ");
-    } else {
+    getchar(); // Get the newline character
+    // Check encode, decode, or invalid input
+    if (ch != 'e' && ch != 'd') {
         printf("Valid options are 'e' to encode or 'd' to decode\n");
-        return 0;
+        return 1;
+    }
+
+    printf("Enter the string to %scode: ", ch == 'e' ? "en" : "de");
+
+    // Read in the message
+    while ((len < MAX_LENGTH - 1) && ((arr_message[len] = getchar()) != '\n')) {
+        len++;
+    }
+    arr_message[len] = '\0'; // Null-terminate 
+
+    for (int i = 0; arr_message[i] != '\0'; i++) {
+        if (arr_message[i] < 'a' || arr_message[i] > 'z') { // Check if outside of 'a' to 'z'
+            printf("Message should only contain alphabet characters, lowercase\n");
+            return 1; 
+        }
     }
 
     // For loop to print out the table
-    printf("\n");
     for (int i = 0; i < 26; i++) {
-        printf("n = %d: ", i);
-        for (int j=0; j < len; j++) {
-            printf("%c", arr_codes[j]);
-        }
-        printf("\n");
+        processMessage(arr_message, temp_message, len, i, ch);
+        printf("n = %2d : %s\n", i, temp_message);
     }
 
     return 0;
