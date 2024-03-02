f62a938ac35892b51055d268cde10f3b1fd20d78
khoai <khanhphuongdo28@gmail.com>
2024-03-01 18:22:09 -0600

    the for loop that print the table out

--- HW5/caesarcipher.c
+++ HW5/caesarcipher.c
@@ -10,31 +10,51 @@
 
 // Helper functions
 // A function that check if input message is correct: lowercase, char, no symbol or num
-// A function that do the encoding process to 1 char a
-// A function that do the decoding process to 1 char b
+char encodeChar(char ch, int key);
+char decodeChar(char ch, int key);
 // A function that either takes in a or b to do the encoding/decoding process to the whole message by returning the table
 // A function that takes in the function that encode/decode the whole message to print out the table
+
+// Function to encode a character using the Caesar cipher
+char encodeChar(char ch, int key) {
+    return ((ch - 'a' + key) % 26) + 'a';
+}
+
+// Function to decode a character using the Caesar cipher
+char decodeChar(char ch, int key) {
+    return ((ch - 'a' - key + 26) % 26) + 'a';
+}
 
 int main(void) {
 
     // Initialize variables
+    char arr_codes[26] = {0};
+    int len = 0;
+    char ch;
 
-    // Print welcoming message
-    printf("This program encrypts and decrypts messages using the Caesar Cipher.\n");
+    // Print welcoming message and read in user input
+    printf("This program encrypts and decrypts messages using the Caesar Cipher\n");
     printf("Type e to encode or d to decode a message: ");
+    ch = getchar();
     // Boolean to see if getchar() records an 'e' or a 'd' and run that following function.
-    /* if ((ch = getchar()) == 'e') {
+    if (ch == 'e') {
         printf("Enter the string to encode: ");
-        encode_function;
-    } else if ((ch = getchar()) == 'd') {
+    } else if (ch == 'd') {
         printf("Enter the string to decode: ");
     } else {
-        printf("Valid options are 'e' to encode or 'd' to decode");
+        printf("Valid options are 'e' to encode or 'd' to decode\n");
         return 0;
     }
-    */
 
     // For loop to print out the table
+    printf("\n");
+    for (int i = 0; i < 26; i++) {
+        printf("n = %d: ", i);
+        for (int j=0; j < len; j++) {
+            printf("%c", arr_codes[j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
