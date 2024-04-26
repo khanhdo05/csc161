8126e1a4740629b684c9a040bca88ce9b7188784
khoai <khanhphuongdo28@gmail.com>
2024-04-26 09:19:19 -0500

    initial commit

---
+++ labs/linked-list-variations/sorted/main.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "string_list.h"
+
+/**
+ * Check to see if a line of input matches a command followed by a singe string argument.
+ * 
+ * \param cmd The command to check against
+ * \param line The line being checked
+ * \returns NULL if there is no match, or a pointer to the argument if the command does match.
+ */
+const char* check_command(const char* cmd, const char* line) {
+  // If the line doesn't begin with cmd, return an error
+  if (strncmp(cmd, line, strlen(cmd)) != 0) return NULL;
+
+  // Check for a space after the command
+  if (line[strlen(cmd)] != ' ') {
+    printf("Error: Missing argument.\n");
+    return NULL;
+  }
+
+  // Make sure the argument isn't empty
+  if (strlen(line) < strlen(cmd) + 2) {
+    printf("Error: Empty argument.\n");
+    return NULL;
+  }
+
+  // Return the argument
+  return line + strlen(cmd) + 1;
+}
+
+void show_help() {
+  printf("Commands:\n");
+  printf("  help: show this message\n");
+  printf("  insert <str>: insert a string at the front of the list\n");
+  printf("  append <str>: append a string to the end of the list\n");
+  printf("  count <str>: count the number of occurrences of a string in the list\n");
+  printf("  remove <str>: remove the first occurrence of a string in the list\n");
+  printf("  length: get the length of the list\n");
+  printf("  print: print all strings in the list\n");
+  printf("  exit: exit the program\n");
+}
+
+int main() {
+  // Create a list and initialize it
+  string_list_t lst;
+  string_list_init(&lst);
+
+  printf("Enter a command. Type \"help\" to see available options.\n");
+
+  // Print a command prompt line:
+  printf("> ");
+  fflush(stdout);
+
+  // Use getline to read commands from the user
+  char* line = NULL;
+  size_t line_capacity = 0;
+  size_t line_length;
+  while ((line_length = getline(&line, &line_capacity, stdin)) != -1) {
+    // Remove the trailing newline from line by overwriting it with a null terminator
+    line[line_length-1] = '\0';
+
+    // A temporary string used for some command handling
+    const char* arg;
+
+    // Check for commands
+    if (strcmp(line, "help") == 0) {
+      // Show a help message
+      show_help();
+
+    } else if (strcmp(line, "length") == 0) {
+      // Print the list length
+      size_t len = string_list_length(&lst);
+      printf("length = %lu\n", len);
+
+    } else if (strcmp(line, "print") == 0) {
+      // Print the list
+      string_list_print(&lst);
+
+    } else if ((arg = check_command("insert", line)) != NULL) {
+      // Insert a new string
+      // Use strdup because the list takes ownership of arg
+      string_list_insert(&lst, strdup(arg));
+    
+    } else if ((arg = check_command("append", line)) != NULL) {
+      // Append a new string
+      // Use strdup because the list takes ownership of arg
+      string_list_append(&lst, strdup(arg));
+    
+    } else if ((arg = check_command("count", line)) != NULL) {
+      // Count occurrences of a value in the list
+      size_t count = string_list_count(&lst, arg);
+      printf("Found %lu occurrence(s)\n", count);
+    
+    } else if ((arg = check_command("remove", line)) != NULL) {
+      // Remove the first occurrence of a value in the list
+      bool result = string_list_remove(&lst, arg);
+      if (!result) {
+        printf("Value was not found in the list.\n");
+      }
+    
+    } else if (strcmp(line, "exit") == 0) {  
+      // Exit the command loop
+      break;
+    
+    } else {
+      printf("Unrecognized command.\n");
+      show_help();
+    }
+
+    // Print a command prompt line:
+    printf("> ");
+    fflush(stdout);
+  }
+
+  // Clean up after getline
+  free(line);
+
+  // Clean up the list
+  string_list_destroy(&lst);
+
+  return 0;
+}
---
+++ labs/linked-list-variations/sorted/string_list.c
@@ -0,0 +1,167 @@
+#include "string_list.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Initialize an empty linked list that holds strings.
+ * 
+ * \param lst A pointer to a string_list_t that should be initialized.
+ */
+void string_list_init(string_list_t* lst) {
+  // The list has no nodes, so head points to NULL
+  lst->head = NULL;
+}
+
+/**
+ * Destroy a linked list that holds strings.
+ * 
+ * \param lst A pointer to an initialized string_list_t that should be destroyed.
+ */
+void string_list_destroy(string_list_t* lst) {
+  string_node_t *curr;
+  curr = lst->head;
+  while (curr != NULL) {
+    curr = curr->next;
+    free(lst->head->value);
+    free(lst->head);
+    lst->head = curr;
+  }
+  lst->head = NULL;
+}
+
+/**
+ * Add a string to the front of a string list.
+ * 
+ * \param lst A pointer to an initialized string list. The new value is added to the front of this
+ *            list. This function does NOT take ownership of lst.
+ * \param str A heap-allocated string that should be inserted into the list. This function takes 
+ *            ownership of str, and will free the underlying memory in string_list_destroy.
+ */
+void string_list_insert(string_list_t* lst, char* str) {
+  string_node_t *new_node;
+  new_node = malloc(sizeof(string_node_t));
+  if (new_node == NULL) {
+    printf("Error: malloc failed.");
+    exit(EXIT_FAILURE);
+  }
+  new_node->value= str; 
+  new_node->next = lst->head;
+  lst->head = new_node;
+}
+
+/**
+ * Add a string to the end of a string list.
+ * 
+ * \param lst A pointer to an initialized string list. The new value is added to the back of this
+ *            list. This function does NOT take ownership of lst.
+ * \param str A heap-allocated string that should be appended to the list. This function takes 
+ *            ownership of str, and will free the underlying memory in string_list_destroy.
+ */
+void string_list_append(string_list_t* lst, char* str) {
+  string_node_t *new_node, *curr;
+  new_node = malloc(sizeof(string_node_t));
+  if (new_node == NULL) {
+    printf("Error: malloc failed.");
+    exit(EXIT_FAILURE);
+  }
+  curr = lst->head;
+  new_node->value = str;
+  new_node->next = NULL;
+  while (curr->next != NULL) {
+    curr = curr->next;
+  }
+  curr->next = new_node;
+}
+
+/**
+ * Count the number of times a value appears in the given list.
+ * 
+ * \param lst A pointer to an initialized string list.
+ * \param str Count the number of times a value matching this string appears in the list.
+ * \returns the number of times str was found in lst
+ */
+size_t string_list_count(const string_list_t* lst, const char* str) {
+  int count = 0;
+  string_node_t *curr;
+  curr = lst->head;
+  while (curr != NULL) {
+    if (strcmp(curr->value, str) == 0) {
+      count += 1;
+    }
+    curr = curr->next;
+  }
+  return count;
+}
+
+/**
+ * Remove a string from the list.
+ * 
+ * \param lst A pointer to an initialized string list. The first matching occurrence of str in this 
+ *            list will be removed.
+ * \param str The first exact match of this string should be removed from the list.
+ * \returns true if the value was found and removed, and false otherwise
+ */
+bool string_list_remove(string_list_t* lst, const char* str) {
+    string_node_t *prev = NULL;
+    string_node_t *curr = lst->head;
+
+    // Traverse the list to find the node to remove
+    while (curr != NULL && strcmp(curr->value, str) != 0) {
+        prev = curr;
+        curr = curr->next;
+    }
+
+    // When the str input is not found, return false
+    if (curr == NULL) {
+        return false;
+    }
+
+    // Update the pointers to remove the node from the list
+    if (prev == NULL) {
+        // If the node found is the head
+        lst->head = curr->next;
+    } else {
+        // If the node found is in the middle
+        prev->next = curr->next;
+    }
+
+    free(curr->value);
+    free(curr);
+
+    return true;
+}
+
+
+/**
+ * Calculate the length of a string list
+ * 
+ * \param lst A pointer to an initialized string list.
+ * \returns the length of the list
+ */
+size_t string_list_length(const string_list_t* lst) {
+  int count = 0;
+  string_node_t *curr;
+  curr = lst->head;
+  while (curr != NULL) {
+    count += 1;
+    curr = curr->next;
+  }
+  return count;
+}
+
+/**
+ * Print the elements of a string list, one per line.
+ * 
+ * \param lst A pointer to an initialized string list that should be printed.
+ */
+void string_list_print(const string_list_t* lst) {
+  string_node_t *curr;
+  curr = lst->head;
+  while (curr != NULL) {
+    printf("%s\n", curr->value);
+    curr = curr->next;
+  }
+}
