179e6f4727524fc7754e93719a5bda8f918d6aeb
khoai <khanhphuongdo28@gmail.com>
2024-04-29 15:51:32 -0500

    commit

--- HW8/paranoia.c
+++ HW8/paranoia.c
@@ -5,8 +5,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(void) {
 
+int main(void) {
+    // Create a list and initialize it
+    player_list_t lst;
+    player_list_init(&lst);
+
+    tagged_list_t tagged;
+    tagged_list_init(&tagged);
+
+    char* line = NULL;
+    size_t line_capacity = 0;
+    size_t line_length;
+    while ((line_length = getline(&line, &line_capacity, stdin)) != -1) {
+        // Remove the trailing newline from line by overwriting it with a null terminator
+        line[line_length-1] = '\0';
+
+        // A temporary string used for some command handling
+        const char* arg;
+
+        // Check for commands
+        if (strcmp(line, "\n") == 0) {
+            break;
+        } 
+    }
+
+    // Clean up after getline
+    free(line);
+
+    // Clean up the list
+    player_list_destroy(&lst);
 
     return 0;
 }
--- HW8/player_list.h
+++ HW8/player_list.h
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+
 // The type for a node in a string list
 typedef struct player {
   char* value;
@@ -12,7 +13,8 @@ typedef struct player {
 // A string list
 typedef struct player_list {
   player_t* head;
-} player_list_t;
+  size_t length;
+} player_list_t, tagged_list_t;
 
 // Initialize an empty list. The lst parameter must point to usable memory that can hold a player_list_t.
 void player_list_init(player_list_t* lst);
@@ -20,14 +22,20 @@ void player_list_init(player_list_t* lst);
 //Free all memory allocated as part of the provided list.
 void player_list_destroy(player_list_t* lst);
 
+//Free all memory allocated as part of the provided list.
+void tagged_list_destroy(tagged_list_t* tagged);
+
 // Add a player to the end of the given list. This function should take ownership of the memory pointed to by the name parameter.
 void player_list_append(player_list_t* lst, char* name);
 
 // Remove the player with the provided name from the list. Return true if a matching player was found and removed, or false otherwise.
-bool player_list_remove(player_list_t* lst, char* name);
+bool player_list_remove(player_list_t* lst, tagged_list_t* tagged, char* name);
 
 // Return the number of players in the given list.
 size_t player_list_length(const player_list_t* lst);
 
 // Print the current list, interpreting it as the target ring. 
 void print_as_target_ring(const player_list_t* lst);
+
+// Print the current tagged list
+void print_as_tagged_list(const tagged_list_t* lst);
