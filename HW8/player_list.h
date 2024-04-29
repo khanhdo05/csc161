18e966f6aebd6b3b2e7b75922e16bb74bddb4507
khoai <khanhphuongdo28@gmail.com>
2024-04-29 00:23:48 -0500

    create a linked list that store lists of player and declarations for all helper functions

---
+++ HW8/player_list.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+// The type for a node in a string list
+typedef struct player {
+  char* value;
+  struct player* next;
+} player_t;
+
+// A string list
+typedef struct player_list {
+  player_t* head;
+} player_list_t;
+
+// Initialize an empty list. The lst parameter must point to usable memory that can hold a player_list_t.
+void player_list_init(player_list_t* lst);
+
+//Free all memory allocated as part of the provided list.
+void player_list_destroy(player_list_t* lst);
+
+// Add a player to the end of the given list. This function should take ownership of the memory pointed to by the name parameter.
+void player_list_append(player_list_t* lst, char* name);
+
+// Remove the player with the provided name from the list. Return true if a matching player was found and removed, or false otherwise.
+bool player_list_remove(player_list_t* lst, char* name);
+
+// Return the number of players in the given list.
+size_t player_list_length(const player_list_t* lst);
+
+// Print the current list, interpreting it as the target ring. 
+void print_as_target_ring(const player_list_t* lst);
